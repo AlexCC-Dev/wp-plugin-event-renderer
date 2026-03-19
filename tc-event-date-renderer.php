9f34854ef9fdd67722c5ced05257f3d4741a9c72
AlexCC-Dev <acarrillo@gruponissi.mx>
2026-03-19 18:02:54 -0500

    Recuperación de versión estables. Alex

--- tc-event-date-renderer.php
+++ tc-event-date-renderer.php
@@ -2,8 +2,8 @@
 /**
  * Plugin Name: Event Date Bridge for Tickera & WooCommerce
  * Description: Interfaz asíncrona para renderizar fechas de Tickera y cargar el checkout en un modal.
- * Version: 1.6.2
- * Author: Jesus Carrillo
+ * Version: 1.0.1
+ * Author: Pospago
  */
 
 if ( ! defined( 'ABSPATH' ) ) exit; // Seguridad
@@ -54,4 +54,3 @@ function tc_get_exact_product_stock_callback() {
     
     wp_send_json_error( 'Producto no encontrado' );
     wp_die();
-}
